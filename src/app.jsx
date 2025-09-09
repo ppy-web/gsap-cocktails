3a761cb5746dbcf252ca2abacc7cb78a4673ce67
李德永 <lideyong@newcapec.net>
2025-09-09 11:41:25 +0800

    ✨ feat: 完成动画效果

--- src/app.jsx
+++ src/app.jsx
@@ -3,6 +3,11 @@ import { gsap } from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Cocktails from './components/Cocktails';
+import About from './components/About';
+import Art from './components/Art';
+import Menu from './components/Menu';
+import Contact from './components/Contact';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -11,7 +16,11 @@ const App = () => {
     <main>
       <Navbar />
       <Hero />
-      <div className="h-dvh bg-black"></div>
+      <Cocktails />
+      <About />
+      <Art />
+      <Menu />
+      <Contact />
     </main>
   );
 }
